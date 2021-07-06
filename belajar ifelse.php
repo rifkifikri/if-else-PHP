2cd8159ff10bfc8a622baf7c6b096039b4bbdf79
rifkifikri <rifkifikri000@gmail.com>
2021-07-06 21:01:58 +0700

    tambahan keteranga nilai

--- belajar ifelse.php
+++ belajar ifelse.php
@@ -3,13 +3,18 @@
 $nilai1 =50;
 $nilai2 = 10;
 
-$hasil=$nilai1%$nilai2;
+$hasil=$nilai1%$nilai2;//operasi aritmatika hampir  sama dengan c++
 $hasil2=$nilai1/$nilai2;
+
+echo "Nilai pertama :",$nilai1,"<br/>";//operasi modulus sama dengan c++
+echo  "Nilai kedua :",$nilai2,"<br/>";
+
+echo "Hasil dari Pembagian kedua nilai adalah ","<br/>";
 if ($hasil==0){
     echo "nilainya bulat yaitu ",$hasil2;
     echo "<br/>";
 }else{
-    echo "nilai berupa desimal yaitu";
+    echo "nilai berupa desimal yaitu",$hasil2;
     echo "<br/>";
 }
  
